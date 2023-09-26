8e5325ced766f4df740a6b6749c4dd7e555d33eb
$GIT_NAME <fabian.diemand@hotmail.com>
2023-09-26 16:31:19 +0000

    bugfix: add cors to terraform, specify both api keys (for vbc uni bern & volley uni bern)

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -3,8 +3,8 @@ resource "aws_apigatewayv2_api" "api_gw" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins = ["*"]
-    allow_methods = ["GET", "OPTIONS", "*"]
+    allow_origins = var.cors_allow_origins
+    allow_methods = ["GET", "OPTIONS", "HEAD"]
     allow_headers = ["Content-Type", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With", "*"]
     max_age       = 0
   }
