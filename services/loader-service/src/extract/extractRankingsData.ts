1c86570b1f00865c87721febc843620a6b77df9c
devtronaut <69691214+devtronaut@users.noreply.github.com>
2024-11-09 00:58:10 +0100

    Feature/logos (#39)
    * feature(logos): extract logos from games data and extend results, upcoming games and rankings with the logo urls
    
    * fix(makefile): add comment for aws context target
    
    * fix(logo-map): add logos from all teams instead of just hometeams (more effort but more reliable)

--- services/loader-service/src/extract/extractRankingsData.ts
+++ services/loader-service/src/extract/extractRankingsData.ts
@@ -78,7 +78,8 @@ function getRankingData(
             ballQuota: resolveQuota(ballsWon, ballsLost),
         }
 
-        if (!teamLogoMap.has(teamId)) throw new Error(`The team ${team.teamId} does not have a logo in the logo map.`);        
+        if (!teamLogoMap.has(teamId)) throw new Error(`The team ${team.teamId} ${teamCaption} ${team.league.caption} does not have a logo in the logo map.`);        
+
         const teamLogoUrl = teamLogoMap.get(teamId)!;
 
         return {
